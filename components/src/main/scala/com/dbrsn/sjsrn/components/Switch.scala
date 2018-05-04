d1f05cbcb298e73d415674afd3b15d06b8463ce8
Denis Borisenko <dborisenko@gmail.com>
2018-05-04 22:33:55 +0200

    Migrated Switch

---
+++ components/src/main/scala/com/dbrsn/sjsrn/components/Switch.scala
@@ -0,0 +1,105 @@
+package com.dbrsn.sjsrn.components
+
+import japgolly.scalajs.react.component.Js.{ RawMounted, UnmountedWithRawType }
+import japgolly.scalajs.react.{ Children, CtorType, JsComponent }
+
+import scala.scalajs.js
+import scala.scalajs.js.annotation.JSImport
+
+object Switch {
+
+  @js.native
+  @JSImport("react-native", "Switch")
+  object RawComponent extends js.Object
+
+  @js.native
+  trait Props extends View.Props {
+    val value: js.UndefOr[Boolean] = js.undefined
+    val disabled: js.UndefOr[Boolean] = js.undefined
+    val onValueChange: js.UndefOr[js.Function1[Boolean, _]] = js.undefined
+    val tintColor: js.UndefOr[String] = js.undefined
+    val onTintColor: js.UndefOr[String] = js.undefined
+    val thumbTintColor: js.UndefOr[String] = js.undefined
+  }
+
+  object Props {
+    def apply(
+      value: js.UndefOr[Boolean] = js.undefined,
+      disabled: js.UndefOr[Boolean] = js.undefined,
+      onValueChange: js.UndefOr[js.Function1[Boolean, _]] = js.undefined,
+      tintColor: js.UndefOr[String] = js.undefined,
+      onTintColor: js.UndefOr[String] = js.undefined,
+      thumbTintColor: js.UndefOr[String] = js.undefined,
+      onStartShouldSetResponder: js.UndefOr[js.Function] = js.undefined,
+      onMoveShouldSetResponder: js.UndefOr[js.Function] = js.undefined,
+      onStartShouldSetResponderCapture: js.UndefOr[js.Function] = js.undefined,
+      onMoveShouldSetResponderCapture: js.UndefOr[js.Function] = js.undefined,
+      onResponderGrant: js.UndefOr[js.Function] = js.undefined,
+      onResponderReject: js.UndefOr[js.Function] = js.undefined,
+      onResponderMove: js.UndefOr[js.Function] = js.undefined,
+      onResponderTerminationRequest: js.UndefOr[js.Function] = js.undefined,
+      onResponderTerminate: js.UndefOr[js.Function] = js.undefined,
+      renderToHardwareTextureAndroid: js.UndefOr[Boolean] = js.undefined,
+      onResponderRelease: js.UndefOr[js.Function] = js.undefined,
+      onMagicTap: js.UndefOr[js.Function] = js.undefined,
+      collapsable: js.UndefOr[Boolean] = js.undefined,
+      shouldRasterizeIOS: js.UndefOr[Boolean] = js.undefined,
+      onLayout: js.UndefOr[js.Function1[LayoutEvent, _]] = js.undefined,
+      onAccessibilityTap: js.UndefOr[js.Function0[_]] = js.undefined,
+      accessibilityLabel: js.UndefOr[String] = js.undefined,
+      AccessibilityComponentType: js.UndefOr[js.Any] = js.undefined,
+      removeClippedSubviews: js.UndefOr[Boolean] = js.undefined,
+      style: js.UndefOr[js.Any] = js.undefined,
+      accessibilityTraits: js.UndefOr[js.Array[AccessibilityTraits]] = js.undefined,
+      onAcccessibilityTap: js.UndefOr[js.Function] = js.undefined,
+      collapsible: js.UndefOr[Boolean] = js.undefined,
+      accessibilityViewIsModal: js.UndefOr[Boolean] = js.undefined,
+      needsOffscreenAlphaCompositing: js.UndefOr[Boolean] = js.undefined,
+      testID: js.UndefOr[String] = js.undefined,
+      pointerEvents: js.UndefOr[PointerEvents] = js.undefined,
+      importantForAccessibility: js.UndefOr[ImportantForAccessibility] = js.undefined,
+      accessible: js.UndefOr[Boolean] = js.undefined
+    ): Props = js.Dynamic.literal(
+      value = value,
+      disabled = disabled,
+      onValueChange = onValueChange,
+      tintColor = tintColor,
+      onTintColor = onTintColor,
+      thumbTintColor = thumbTintColor,
+      onStartShouldSetResponder = onStartShouldSetResponder,
+      onMoveShouldSetResponder = onMoveShouldSetResponder,
+      onStartShouldSetResponderCapture = onStartShouldSetResponderCapture,
+      onMoveShouldSetResponderCapture = onMoveShouldSetResponderCapture,
+      onResponderGrant = onResponderGrant,
+      onResponderReject = onResponderReject,
+      onResponderMove = onResponderMove,
+      onResponderTerminationRequest = onResponderTerminationRequest,
+      onResponderTerminate = onResponderTerminate,
+      renderToHardwareTextureAndroid = renderToHardwareTextureAndroid,
+      onResponderRelease = onResponderRelease,
+      onMagicTap = onMagicTap,
+      collapsable = collapsable,
+      shouldRasterizeIOS = shouldRasterizeIOS,
+      onLayout = onLayout,
+      onAccessibilityTap = onAccessibilityTap,
+      accessibilityLabel = accessibilityLabel,
+      AccessibilityComponentType = AccessibilityComponentType,
+      removeClippedSubviews = removeClippedSubviews,
+      style = style,
+      accessibilityTraits = accessibilityTraits,
+      onAcccessibilityTap = onAcccessibilityTap,
+      collapsible = collapsible,
+      accessibilityViewIsModal = accessibilityViewIsModal,
+      needsOffscreenAlphaCompositing = needsOffscreenAlphaCompositing,
+      testID = testID,
+      pointerEvents = pointerEvents,
+      importantForAccessibility = importantForAccessibility,
+      accessible = accessible
+    ).asInstanceOf[Props]
+  }
+
+  private val component = JsComponent[Props, Children.Varargs, Null](RawComponent)
+
+  def apply(props: Props)(children: CtorType.ChildArg*): UnmountedWithRawType[Props, Null, RawMounted[Props, Null]] =
+    component(props)(children: _*)
+}
