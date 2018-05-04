e82ae370713477017b5f5ed313fd05be4344466f
Denis Borisenko <dborisenko@gmail.com>
2018-05-05 00:18:21 +0200

    Migrated StyleSheet

--- components/src/main/scala/com/dbrsn/sjsrn/api/StyleSheet.scala
+++ components/src/main/scala/com/dbrsn/sjsrn/api/StyleSheet.scala
@@ -1,37 +1,35 @@
-package sri.universal.apis
+package com.dbrsn.sjsrn.api
+
+import com.github.ghik.silencer.silent
 
 import scala.scalajs.js
 import scala.scalajs.js.annotation.JSImport
 
+@silent
 @js.native
 @JSImport("ReactNativePropRegistry", JSImport.Namespace)
 object ReactNativePropRegistry extends js.Object {
-
   def register(obj: js.Object): Int = js.native
 
   def getByID(id: Int): js.Object = js.native
 }
 
+@silent
 @js.native
 @JSImport("StyleSheetValidation", JSImport.Namespace)
 object StyleSheetValidation extends js.Object {
-
   def register(obj: js.Object): Int = js.native
 
   def getByID(id: Int): js.Object = js.native
 }
 
+@silent
 @js.native
-trait StyleSheet extends js.Object {
-
+@JSImport("react-native", "StyleSheet")
+object StyleSheet extends js.Object {
   def create[T <: js.Object](obj: T): T = js.native
 
   val hairlineWidth: Double = js.native
 
   val absoluteFill: js.Any = js.native
-
 }
-
-@js.native
-@JSImport("react-native", "StyleSheet")
-object StyleSheet extends StyleSheet
